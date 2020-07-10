91ad94cdb8cd2585603ab425a57662c5269add43
doannhatquang27 <doannhatquang27@gmail.com>
2020-07-10 08:24:53 +0700

    1st: the first upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to Nhat Quang repository
+This repo contains the source code of a simple Java application used to demostrate the CI - Continuous Intergation process - a part of Introduction to Software Engineering course at FPT University - HCMC Campus
+
+## You will find in this repo:
+* MathUtil class - holds the list of math methods
+* Test class - demonstrate on Unit Testing and TDD
+* .gitignore
+* Markdown file - this file itself
+
+## Connect me via
+1. [GitHub](https://github.com/doannhatquang27)
+2. [Facebook](https://www.facebook.com/i.love.fish.forever)
+
+#### © 2020 by Nhat Quang
---
+++ src/quang/util/MathUtil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quang.util;
+
+/**
+ *
+ * @author ASUS
+ */
+//đây là class chứa các hàm toán học, tính toán, với mục tiêu xài chung
+//cho các nơi khác, phàm cái gì mang tính xài chung, ta sẽ dùng STATIC
+public class MathUtil {
+    //tính n! = 1.2.3...n. Vì n! tăng giá trị rất nhanh, sớm tràn miền int
+    //tính giai thừa từ 15 trở lại
+    public static long computeFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
