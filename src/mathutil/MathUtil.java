981d06586b6ff86996b094f395cb5661a6235543
doannhatquang27 <doannhatquang27@gmail.com>
2020-07-17 08:25:33 +0700

    3rd: add tdd

--- src/mathutil/MathUtil.java
+++ src/mathutil/MathUtil.java
@@ -16,7 +16,18 @@ public class MathUtil {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //hy vọng hàm trả về 120 nếu tính 5!
         System.out.println("5! = " + computeFactorial(5));
+        
+        //hy vọng hàm trả về 1 nếu tính 1!
+        //expected 1 if cF(1)
+        System.out.println("1! = " + computeFactorial(1));
+        
+        //expected 1 if cF(0)
+        System.out.println("0! = " + computeFactorial(0));
+        
+        //expected Exception if cF(-5)
+        System.out.println("-5! = " + computeFactorial(-5));
     }
     
 }
---
+++ test/tdd/MathUtilTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static quang.util.MathUtil.computeFactorial;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtilTest {
+        
+        @Test //báo cho JVM biết rằng đây là hàm main()
+        //trong hàm này chứa các lệnh để test code chính của mình ở bên MathUtil
+        //ta test các tình huống thành công ở đây, 
+        //thành công: cF(5) -> 120 gọi là thành công 
+        public void testSuccessfulCases() {
+            assertEquals(120, computeFactorial(5));
+            assertEquals(720, computeFactorial(6));
+            assertEquals(1, computeFactorial(0));
+            assertEquals(1, computeFactorial(1));
+            assertEquals(2, computeFactorial(2));
+        }   
+        
+        //annotation, dấu hiệu báo cho JVM và thư viện jar liên quan
+        //biết cần phải làm gì
+        @Test(expected = IllegalArgumentException.class)
+        public void testFailedCases() {
+            //kì vọng ném ra ngoại lệ
+            //ngoại lệ không phải là 1 value để có thể so sánh được
+            //nên ko xài hàm assertEquals() được
+            computeFactorial(-5);
+                
+        }        
+        
+        //code có thể tiềm ẩn lỗi login nhưng Clean&Build luôn ra file .jar
+        //nếu code ko sai cú pháp
+        //mình phải DISABLE CÁI NÚT CLEAN & BUILD NẾU CODE CÒN MÀU ĐỎ
+        //TỨC LÀ CÒN ERROR LOGIN
+        //NHỚ 2 CON SỐ: 1005 Netbeans 8, 1204 Netbeans 10 trở lên
+        //khi chơi với Ant project
+}
