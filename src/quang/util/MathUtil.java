3f0a89a9f0d8505c149269cad569b3e388a98a26
doannhatquang27 <doannhatquang27@gmail.com>
2020-10-06 21:47:13 +0700

    Try to use Jenkins

--- src/quang/util/MathUtil.java
+++ src/quang/util/MathUtil.java
@@ -9,25 +9,35 @@ package quang.util;
  *
  * @author ASUS
  */
-//đây là class chứa các hàm toán học, tính toán, với mục tiêu xài chung
-//cho các nơi khác, phàm cái gì mang tính xài chung, ta sẽ dùng STATIC
+
 public class MathUtil {
-    //tính n! = 1.2.3...n. Vì n! tăng giá trị rất nhanh, sớm tràn miền int
-    //tính giai thừa từ 15 trở lại
     public static long computeFactorial(int n) {
-        if (n < 0 || n > 15) {
-            throw new IllegalArgumentException("Invalid input. n must be >= 0");
-        }
+        if (n < 0 || n > 15) throw new IllegalArgumentException("Invalid input. n must be >= 0");
+        if (n == 0 || n == 1) return 1;
+        return n * computeFactorial(n - 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*đây là class chứa các hàm toán học, tính toán, với mục tiêu xài chung
+cho các nơi khác, phàm cái gì mang tính xài chung, ta sẽ dùng STATIC*/
+
+
+//tính n! = 1.2.3...n. Vì n! tăng giá trị rất nhanh, sớm tràn miền int
+    //tính giai thừa từ 15 trở lại
+
+
 
 //        long result = 1;
 //        for (int i = 1; i <= n; i++) {
 //            result *= i;
 //        }
 //        return result;
-
-        if (n == 0 || n == 1)
-            return 1;
-        
-        return n * computeFactorial(n - 1);
-    }
-}
--- test/tdd/MathUtilTest.java
+++ test/tdd/MathUtilTest.java
@@ -15,33 +15,56 @@ import static quang.util.MathUtil.computeFactorial;
  */
 public class MathUtilTest {
         
-        @Test //báo cho JVM biết rằng đây là hàm main()
-        //trong hàm này chứa các lệnh để test code chính của mình ở bên MathUtil
-        //ta test các tình huống thành công ở đây, 
-        //thành công: cF(5) -> 120 gọi là thành công 
+        @Test 
         public void testSuccessfulCases() {
             assertEquals(120, computeFactorial(5));
-            assertEquals(721, computeFactorial(6));
+            assertEquals(720, computeFactorial(6));
             assertEquals(1, computeFactorial(0));
             assertEquals(1, computeFactorial(1));
             assertEquals(2, computeFactorial(2));
         }   
         
-        //annotation, dấu hiệu báo cho JVM và thư viện jar liên quan
-        //biết cần phải làm gì
         @Test(expected = IllegalArgumentException.class)
         public void testFailedCases() {
-            //kì vọng ném ra ngoại lệ
-            //ngoại lệ không phải là 1 value để có thể so sánh được
-            //nên ko xài hàm assertEquals() được
             computeFactorial(-5);
-            computeFactorial(16); //giai thua tang rat nhanh, khong choi qua 15
+            computeFactorial(16); 
         }        
         
-        //code có thể tiềm ẩn lỗi login nhưng Clean&Build luôn ra file .jar
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//báo cho JVM biết rằng đây là hàm main()
+        //trong hàm này chứa các lệnh để test code chính của mình ở bên MathUtil
+        //ta test các tình huống thành công ở đây, 
+        //thành công: cF(5) -> 120 gọi là thành công 
+
+
+
+//annotation, dấu hiệu báo cho JVM và thư viện jar liên quan
+        //biết cần phải làm gì
+
+
+
+//kì vọng ném ra ngoại lệ
+            //ngoại lệ không phải là 1 value để có thể so sánh được
+            //nên ko xài hàm assertEquals() được
+
+
+
+//code có thể tiềm ẩn lỗi login nhưng Clean&Build luôn ra file .jar
         //nếu code ko sai cú pháp
         //mình phải DISABLE CÁI NÚT CLEAN & BUILD NẾU CODE CÒN MÀU ĐỎ
         //TỨC LÀ CÒN ERROR LOGIN
         //NHỚ 2 CON SỐ: 1005 Netbeans 8, 1204 Netbeans 10 trở lên
         //khi chơi với Ant project
-}
